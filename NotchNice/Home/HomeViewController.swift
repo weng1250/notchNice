17ae8b4a1d83c0ca381ff05ce753826b59141b0c
WengZilin <wzl@meitu.com>
2020-05-11 23:52:23 +0800

    去掉广点通，编译通过

--- NotchNice/Home/HomeViewController.swift
+++ NotchNice/Home/HomeViewController.swift
@@ -7,13 +7,48 @@
 //
 
 import UIKit
+import ZLKit
 
 class HomeViewController: UIViewController {
-
+    // MARK: - Public
+    
+    
+    // MARK: - Lift
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupView()
     }
     
+    private func setupView() {
+        
+//        let k 
+    }
+    
+    
+    // MARK: - UI
+    @IBOutlet weak var wallPaperImageView: UIImageView!
+    @IBOutlet weak var applePreviewImageView: UIImageView!
+    @IBOutlet weak var menuStackView: UIStackView!
+    
+    
+    @IBAction func actionAlbumBtn(_ sender: Any) {
+    }
+    
+    @IBAction func actionBangBtn(_ sender: Any) {
+    }
+    
+    @IBAction func actionWallPaperBtn(_ sender: Any) {
+    }
+    
+    @IBAction func actionApplePreviewBtn(_ sender: Any) {
+    }
+    
+    @IBAction func actionSaveBtn(_ sender: Any) {
+    }
+    
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+        
+    }
+    
+    // MARK: - Private
 }
