b39abd25304cc881b4c77169789abf1c5944f656
WengZilin <wzl@meitu.com>
2020-05-14 22:32:37 +0800

    添加壁纸列表视图

--- NotchNice/Home/Cell/WallpaperCollectionViewCell.swift
+++ NotchNice/Home/Cell/WallpaperCollectionViewCell.swift
@@ -10,4 +10,25 @@ import UIKit
 
 class WallpaperCollectionViewCell: UICollectionViewCell {
     
+    func update(with bangModel: WallpaperModel, selected: Bool) {
+        selectedFlagImageView.isHidden = !selected
+        let image = UIImage.init(name: bangModel.id)
+        wallpaperImageView.image = image
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor.black.withAlphaComponent(0.8).cgColor
+        layer.borderWidth = 1
+        layer.cornerRadius = 2
+        layer.masksToBounds = true
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    @IBOutlet weak var wallpaperImageView: UIImageView!
+    @IBOutlet weak var selectedFlagImageView: UIImageView!
 }
--- NotchNice/Home/WallPaperListVC.swift
+++ NotchNice/Home/WallPaperListVC.swift
@@ -12,8 +12,47 @@ class WallPaperListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 让毛玻璃能超出区域
+        self.view.clipsToBounds = false
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.reloadData()
+    }
+    // MARK: - UI
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - Private
+       private var selectedWallpaperModel: WallpaperModel? = nil
+       private lazy var items: [WallpaperModel] = {
+           if let list = WallpaperDataSource.shared.wallpaperListModels {
+               return list
+           }
+           return []
+       }()
+}
 
-        // Do any additional setup after loading the view.
+extension WallPaperListVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
     }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! WallpaperCollectionViewCell
+        var selected = false
+        let model = items[indexPath.item]
+        if let selectedModel = self.selectedWallpaperModel, model == selectedModel {
+            selected = true
+        }
+        cell.update(with: model, selected: selected)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // 缩进
+        let model = items[indexPath.item]
+        selectedWallpaperModel = model
+        collectionView.reloadData()
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
 }
