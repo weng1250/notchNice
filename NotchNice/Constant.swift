e11329c644c228fa70981d3ba30a98de616d40ca
WengZilin <wzl@meitu.com>
2020-05-20 00:09:08 +0800

    添加主题色

---
+++ NotchNice/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  NotchNice
+//
+//  Created by weng on 2020/5/19.
+//  Copyright © 2020 com.xuzhanfen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// 参考配色https://www.uibaba.com/article/20160208/365.html
+let NotchMainThemeColorGreenBlueColor = UIColor.init(red: 0, green: 194.0 / 255, blue: 222.0 / 255, alpha: 1)
+
+let NotchMainThemeColorOrangeColor = UIColor.init(red: 1, green: 187.0 / 255, blue: 44.0 / 255, alpha: 1)
