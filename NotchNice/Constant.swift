51fc47c643a3d11b84c796718f2912451e04461d
WengZilin <wzl@meitu.com>
2020-05-22 00:13:07 +0800

    添加订阅信息

--- NotchNice/Constant.swift
+++ NotchNice/Constant.swift
@@ -13,3 +13,9 @@ import UIKit
 let NotchMainThemeColorGreenBlueColor = UIColor.init(red: 0, green: 194.0 / 255, blue: 222.0 / 255, alpha: 1)
 
 let NotchMainThemeColorOrangeColor = UIColor.init(red: 1, green: 187.0 / 255, blue: 44.0 / 255, alpha: 1)
+
+class SubscribeInfo {
+    static let monthlySubscribeAppleID      = "1514624203"
+    static let monthlySubscribeProductID    = "NotchGuy_month_subscribe"
+    static let monthlySubscribeGroup        = "NotchGuy_subscribe_group"
+}
